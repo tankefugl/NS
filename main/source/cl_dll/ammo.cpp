ac9b4c8b25157a740290179e3d0abf81b45bca84
pierow <pierow8@gmail.com>
2021-01-31 18:59:56 -0500

    weaponcfgs update

--- main/source/cl_dll/ammo.cpp
+++ main/source/cl_dll/ammo.cpp
@@ -697,8 +697,10 @@ void CHudAmmo::Think(void)
 	if (gHUD.GetCurrentWeaponID() != gWR.LastWeaponId)
 	{
 		gWR.LastWeaponId = gHUD.GetCurrentWeaponID();
-		WEAPON* currentWeapon = gWR.GetWeapon(gHUD.GetCurrentWeaponID());
 
+		ClientCmd("exec weaponcfgs/default.cfg");
+
+		WEAPON* currentWeapon = gWR.GetWeapon(gHUD.GetCurrentWeaponID());
 		char weapcfg[128];
 		sprintf(weapcfg, "exec weaponcfgs/%s.cfg", currentWeapon->szName);
 		ClientCmd(weapcfg);
